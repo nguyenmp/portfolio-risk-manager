c758242ad6fff644840bb5e8a962e385e8c3d338
Mark Nguyen <mnguyen4@apple.com>
2017-12-31 19:04:53 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# portfolio-risk-manager
-Tools and thoughts on managing high risk assets
+# Portfolio Risk Manager
+
+This project's goal is to set up a part of my portfolio as cryptocurrencies.  For such a high risk investment avenue, I will aim to reduce risk using several ideas from the Wealthfront white paper, namely time-cost averaging and maintaining a market-cap index fund like asset portfolio of cryptocoins as an asset class that will maintain some asset diversification percentage against my standard portfolio.
+
+# Wallets
+
+I have a couple of options:
+
+1. Roll My Own
+2. Use a wallet per coin
+3. Use a multi-coin wallet
+
+Each of these have their own flaws like:
+
+* trusting a third party or
+* trusting myself to write it correctly and safely. 
+
+Do I store wallets encrypted in the cloud so I don't lose it?  I surely don't trust third party wallets like exchanges.  Rolling my own is super sketchy but many third party ones seem prone to errors like sending to the wrong address or another coin's address.  I can at least put up guards against my own mistakes that I can immediately tell I will make.  Also many third party wallets have documentation and support pages that are hard to read which makes me trust them significantly less.
+
+# Exchanges
+
+## Fees
+
+Each exchange has their own fees and they all seem to float around.   One concern I have is as I'm calculating my moves to make, the fees could either be lies or they could be changing from under me.
+
+Even if I have accurate data on fees being charged, they are different from each other and can range from:
+
+* Free
+* A percentage on the transaction content
+* A flat rate
+
+I would really like to store all my money in my own wallet instead of doing it on the exchanges but that might not be possible with all the fees.  I should run the numbers to figure this stuff out.
+
+### Deposit Fees from USD
+
+Deposit Fees are a concern when the overall crypto coin market is decreasing or is stable, as my overall portfolio size will increase and I will need to add funds to maintain my spread of investments.  For example, if I have 10% of my portfolio in cryptocoins, and I receive a windfall of some amount, I need to add 10% of that windfall to cryptocoins to maintain my spread.  This will be common because I will regularly be paid for my day job.
+
+### Withdrawl Fees from USD
+
+Withdrawl fees are a concern when the overall cryptocoin market is increasing w.r.t. the rest of my portfolio or I need to reallocate my funds into cash on hand for a big purchase.  This will probably be less comon as I don't expect any bit purchases.
+
+### Fees for Trading
+
+It also seems like exchanges generally charge a fee for trading between coins.
+
+### Market Data
+
+
+| Exchange Name | Deposit Fee | Withdrawl Fee | Trade Fee |
+| ------------- | -----------:| -------------:| ---------:|
+| Binance       | Free        | 25 USDT       | 0.1%      |
+| Bittrex       | Free        | Free          | 0.25%     |
+| Bithumb       | Free        | 28 USD-BTC    | 0.15%     |
+| Poloniex      | Free        | 28 USD-BTC    | 0.15%     |
+
+## Volume as Market Cap
+
+## API
+
+If the exchange has an API, all the better, otherwise, I'll need to manually rebalance through their wallet or website which might be scary since human error seems so easy.
+
+## Trustworthiness
+
+Exchanges are basically unregulated so I'm not sure if I can really trust their exchange and volume data or even their data on fees because those things can change dramatically.  During transactions between coins and to USDs, I should be very careful to doublecheck the books and make sure that I'm being paid fairly and that I'm not being stripped of my coins or being overcharged with fees.
+
+# Crypto Coins
+
+Finally, I need to decide which cryptocoins to invest in and how much of each should I be holding onto.
+
+## Volume Traded as Market Cap
+
+I think this can dual purpose as both which coins to invest in as well as how much of each coin to invest into.   The former is justifiable because volume traded as market cap is an indication of how active this currency is being used.  The latter is justifiable because my portfolio will nautrally follow the market and I won't need to rebalance the index.  I just need to buy and sell as they enter and leave the index.
+
+# Future Work
+
+## Would I have invested into crypto coins before their meteoric rise?
+
+## How volatile is the ranking of cryptocoins by volume as market cap in USD?
+
+## How volatile is the ranking of exchanges by volume as market cap in USD?
---
+++ show_top_volumes.py
@@ -0,0 +1,192 @@
+'''
+This little script downloads the market price and volume traded day to day to
+show how the top coins change over time.  The intention is to analyze how this
+measure behaves given historical data.
+'''
+
+import datetime
+import logging
+import json
+import multiprocessing
+import pprint
+import re
+import sys
+import time
+
+import requests
+
+API_KEY = '0BbG9UxZhOvmEHdfSHAOtUlAN3RoBd'
+HOSTNAME = 'https://www.worldcoinindex.com'
+
+LOGGER = logging.getLogger()
+
+DELAY_FOR_INVITATION = 10*5
+DELAY_FOR_EVICTION = 7*5
+
+
+def configure_logging():
+    '''
+    Configures logging for this module
+    '''
+    LOGGER.setLevel(logging.DEBUG)
+    channel = logging.StreamHandler(sys.stdout)
+    channel.setLevel(logging.DEBUG)
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    channel.setFormatter(formatter)
+    LOGGER.addHandler(channel)
+
+
+def get_markets():
+    '''
+    Gives the current known cryptocoins and their market rate
+    '''
+    endpoint = 'apiservice/getmarkets'
+    params = {
+        'fiat': 'usd',
+        'key': API_KEY,
+    }
+    url = '{}/{}'.format(HOSTNAME, endpoint)
+    response = requests.get(url, params)
+    response.raise_for_status()
+    return json.loads(response.text)['Markets'][0]
+
+
+def get_coin_names():
+    '''
+    Fetches all the tickers of the coins being tracked
+    '''
+    LOGGER.debug('Getting coin names')
+    markets = get_markets()
+    return [re.sub(r'\/BTC$', '', market['Name']) for market in markets]
+
+
+def get_history(name, days=30):
+    '''
+    Returns historical data given the name of the coin
+    '''
+    LOGGER.debug('Getting history for %s', name)
+    endpoint = 'home/GetGraphdatatesttest'
+    headers = {
+        'Referer': 'https://www.worldcoinindex.com/coin/dogecoin',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+    payload = {
+        'marketid': name,
+        'days': days,
+    }
+
+    url = '{}/{}'.format(HOSTNAME, endpoint)
+    response = requests.post(url, payload, headers=headers)
+    response.raise_for_status()
+    return json.loads(response.text)
+
+
+def get_histories(cache=None):
+    if cache:
+        LOGGER.debug('Loading from cache')
+        with open('histories.json', 'r') as file_handle:
+            histories = json.loads(file_handle.read())
+        LOGGER.debug('Loaded from cache')
+        return histories
+
+    pool = multiprocessing.Pool(100)
+    return pool.map(get_history, get_coin_names())
+
+
+def analyze(histories):
+    indexes = {}
+    earlier = {}  # Label to Volume Market Cap Rank
+    now = None
+    lru_cache = {}  # Mapping of coin name to the last event we saw it in
+    adder_cache = {}  # Mapping of coin name to the number of times seen
+
+    while True:
+        # Get the next earliest timestamp
+        min_timestamp = time.time()
+        for history in histories:
+            index = indexes.get(history['primaryname'], 0)
+            if index >= len(history['data']):
+                continue  # ? This might need to be a delete
+            event = history['data'][index]
+            if event['Timestamp'] < min_timestamp:
+                min_timestamp = event['Timestamp']
+
+        now = dict(earlier)
+
+        # Get every update at this time
+        for history in histories:
+            index = indexes.get(history['primaryname'], 0)
+            if index >= len(history['data']):
+                continue  # ? This might need to be a delete
+            event = history['data'][index]
+            if event['Timestamp'] == min_timestamp:
+                indexes[history['primaryname']] = index + 1
+                now[history['primaryname']] = event['Volume'] * event['Price']
+
+        readable_time = datetime.datetime.utcfromtimestamp(min_timestamp)
+        LOGGER.debug('Timestamp: %s', readable_time)
+
+        # Ignore early seed data where all values are 0
+        if all([value == 0 for value in now.values()]):
+            continue
+
+        top_now = sorted(
+            now.items(),
+            cmp=lambda a, b: int(a[1] - b[1]),
+            reverse=True
+        )[:20]
+
+        # Degrade the adder cache because of time lapse
+        for name in adder_cache:
+            adder_cache[name] -= 1
+
+        # Add new items to the adder cache or refersh if in the lru_cache
+        for item in top_now:
+            name = item[0]
+            if name in lru_cache:
+                lru_cache[name] = DELAY_FOR_EVICTION
+            else:
+                adder_cache[name] = adder_cache.get(name, 0) + 2
+
+        to_add = set()
+        for name in adder_cache:
+            if adder_cache[name] >= DELAY_FOR_INVITATION:
+                to_add.add(name)
+
+        for name in to_add:
+            lru_cache[name] = DELAY_FOR_EVICTION
+            del adder_cache[name]
+            LOGGER.debug('%s has entered the top listings', name)
+
+        to_delete = set()
+        for name in lru_cache:
+            lru_cache[name] -= 1
+            if lru_cache[name] < 0:
+                LOGGER.debug('%s has dropped off the last 5 times', name)
+                to_delete.add(name)
+
+        if to_delete:
+            LOGGER.debug('Now:\n%s', pprint.pformat(top_now))
+            LOGGER.debug('Last Few:\n%s', pprint.pformat(lru_cache))
+
+        for name in to_delete:
+            del lru_cache[name]
+
+        earlier = dict(now)
+
+
+def main():
+    '''
+    main
+    '''
+    configure_logging()
+    histories = get_histories()
+    import pdb
+    pdb.set_trace()
+    analyze(histories)
+
+
+if __name__ == '__main__':
+    main()
